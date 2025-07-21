8f7d6070bf1df83cf4fe5b20323fcb553cd8b662
el shadai <imanuelsha@gmail.com>
2025-07-22 01:43:50 +0530

    new config

---
+++ src/main/java/com/trader/backend/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.trader.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://frontendfortheautobot.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
