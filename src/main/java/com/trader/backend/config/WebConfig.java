9dd8ea280b4c0ecb0e246378a94cc26d7691ac92
el shadai <imanuelsha@gmail.com>
2025-08-04 13:08:04 +0530

    updated corsconfig

--- src/main/java/com/trader/backend/config/WebConfig.java
+++ src/main/java/com/trader/backend/config/WebConfig.java
@@ -1,14 +1,7 @@
 package com.trader.backend.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
 
-@Configuration
 public class WebConfig {
 
 }
