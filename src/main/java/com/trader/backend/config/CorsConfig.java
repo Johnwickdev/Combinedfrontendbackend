e53f54f286e3e57da986844dfdbea56356cc4bd1
el shadai <imanuelsha@gmail.com>
2025-08-04 15:05:18 +0530

    config changes

--- src/main/java/com/trader/backend/config/CorsConfig.java
+++ src/main/java/com/trader/backend/config/CorsConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 @ComponentScan
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
 
     @Bean
